6e22826a8ec1922115d680c8deae25ad60e40f0c
CesarPalma11 <cesarpalmag16@gmail.com>
2024-01-22 00:48:58 -0300

    photo

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import './home.css'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import { AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
 import {AiOutlineArrowDown, AiFillGithub} from 'react-icons/ai'
 import {GoMail} from 'react-icons/go'
 import {BsWhatsapp} from 'react-icons/bs'
@@ -26,37 +26,45 @@ const Home = () => {
   return (
     <> 
     
-    
     <div className='home'>
-        <div className='content' data-aos="fade-right">
-          <h1>¡Hey👋!<br></br>soy <span style={{color:'yellow'}}>cesar palma</span><br></br> {''}
-          <span style={{color:'yellow'}}>{text}</span>
-          <span style={{color:'yellow'}}>
-            
-            <Cursor  cursorStyle='<' />
-          </span>
-          </h1> 
-          
-        </div>
-        <div className='btn_div'>
-        <p className='pin'>Argentina <LuMapPin className='pin_color' /> </p>
-        <div className='btn'>
-
-          <Link to='contact' spy={true} smooth={true} offset={50} duration={700} className='btn_ct'>Contacto</Link>
-          <a href='/documents/Cesar+Palma+FrontEnd+Developer.pdf' download className='btn_cv' without rel='noreferrer' >Descargar CV </a>
-          </div>
-        </div>
-        <a href='https://wa.me/5491160185717/?text=Hola,%20queria%20comunicarme%20con%20usted!'  className='btn_wsp' without rel="noreferrer" ><BsWhatsapp /> </a>
-        <div className='icons' data-aos="fade-left">
-          <a href='mailto:cesarpalmag16@gmail.com' className='buttom_mail' without rel='noreferrer' target='_blank'><GoMail /></a>
-          <a href='https://www.linkedin.com/in/cesarpalma11/' className='buttom' without rel='noreferrer'><AiFillLinkedin /></a>
-          <a href='https://twitter.com/Palma99969869' className='buttom' without rel='noreferrer'><AiOutlineTwitter /></a>
-          <a href='https://github.com/CesarPalma11' className='buttom' without rel='noreferrer'><AiFillGithub /></a>
-        </div>
-          <div className='arrow'><AiOutlineArrowDown /></div>
-        </div>
         
 
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6'>
+            <img src='./img/photo.jpeg' alt='img' className='img-fluid'/> 
+            <div className='icons'>
+              <a href='mailto:cesarpalmag16@gmail.com' className='buttom_mail' without rel='noreferrer' target='_blank'><GoMail /></a>
+              <a href='https://www.linkedin.com/in/cesarpalma11/' className='buttom' without rel='noreferrer'><AiFillLinkedin /></a>
+              <a href='https://github.com/CesarPalma11' className='buttom' without rel='noreferrer'><AiFillGithub /></a>
+          </div>
+
+
+            </div>
+            <div className='col-md-6'>
+              <div className='move'>
+            <h1>¡Hey👋!<br></br>soy <span style={{color:'yellow'}}>cesar palma</span><br></br> {''}
+          <span style={{color:'yellow'}}>{text}</span>
+          <span style={{color:'yellow'}}>
+          
+            
+            <Cursor  cursorStyle='<' />
+
+            <p className='pin'>Argentina <LuMapPin className='pin_color' /> </p>
+          </span>
+          <div className='btn'>
+          <Link to='contact' spy={true} smooth={true} offset={50} duration={700} className='btn_ct'>Contacto</Link>
+          <a href='/documents/Cesar+Palma+FrontEnd+Developer.pdf' download className='btn_cv' without rel='noreferrer' >Descargar CV </a>
+          
+          </div>
+          </h1> 
+          </div>
+            </div>
+          </div>
+        </div>
+
+        <a href='https://wa.me/5491160185717/?text=Hola,%20queria%20comunicarme%20con%20usted!'  className='btn_wsp' without rel="noreferrer" ><BsWhatsapp /> </a>
+        </div>
     </>
   )
 }
